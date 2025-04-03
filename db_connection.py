c95ffa725d15385e21a310474cd7ef49ce50a47d
Luigi Ajello <luigipedrosoajello@gmail.com>
2025-04-03 20:36:00 -0300

    fix

--- db_connection.py
+++ db_connection.py
@@ -5,12 +5,16 @@ import os
 # Carregar as variáveis do arquivo .env
 load_dotenv()
 
+# Função para remover aspas se necessário (para por streamlit no ar)
+def clean_env_var(var):
+    return var.strip('"').strip("'") if var else var
+
 # Configurações do banco de dados usando as variáveis de ambiente
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-database_name = os.getenv("DB_NAME")
+host = clean_env_var(os.getenv("DB_HOST"))
+port = clean_env_var(os.getenv("DB_PORT"))
+user = clean_env_var(os.getenv("DB_USER"))
+password = clean_env_var(os.getenv("DB_PASSWORD"))
+database_name = clean_env_var(os.getenv("DB_NAME"))
 
 DATABASE_URL = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database_name}'
 engine = create_engine(DATABASE_URL)
